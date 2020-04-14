ca8a19034c3bfd26765520549982ee19e123eefe
Saketh Kollu <saketh.kollu@gmail.com>
2020-04-13 23:12:49 -0700

    Updates

---
+++ precache-manifest.78cf2993dfae93faa22f9c140d1e44d1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a69f77a8ec3d0dbe41ecb25f93ba446a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "88ebf549c8a28e957833",
+    "url": "/static/css/main.f82be82e.chunk.css"
+  },
+  {
+    "revision": "d8e638dd521320bb176f",
+    "url": "/static/js/2.0a9ee395.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.0a9ee395.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "88ebf549c8a28e957833",
+    "url": "/static/js/main.047c25d1.chunk.js"
+  },
+  {
+    "revision": "8b446413bbf45cb334c3",
+    "url": "/static/js/runtime-main.e1107148.js"
+  }
+]);
